9649d4b49d10e653986983817634681a4b494fe2
yoshiaki44 <yo44ch@gmail.com>
2022-03-07 07:55:43 +0900

    stripe.jsの処理。（サーバーサイド）

---
+++ src/pages/api/createProduct.page.ts
@@ -0,0 +1,36 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+type CreateProduct = {
+  productName: string;
+  owner: string;
+  unitAmount: number;
+};
+
+const createProduct = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { productName, owner, unitAmount } = req.body as CreateProduct;
+
+  if (req.method === "POST") {
+    try {
+      const product = await stripe.products.create({
+        name: productName,
+        metadata: {
+          owner,
+        },
+      });
+
+      const price = await stripe.prices.create({
+        unit_amount: unitAmount,
+        currency: "jpy",
+        product: product.id,
+      });
+
+      res.status(200).json(price.id);
+    } catch {
+      res.status(500).json("error");
+    }
+  }
+};
+
+export default createProduct;
