2688cf58a92a9b63b5f982e1eaed46b54184481a
yoshiaki44 <yo44ch@gmail.com>
2022-03-07 07:50:11 +0900

    レイアウト変更とコンポーネント追加

--- src/layout/FixedLayout.tsx
+++ src/layout/FixedLayout.tsx
@@ -1,4 +1,5 @@
 import type { CustomLayout } from "next";
+import { Profile } from "src/layout/Profile";
 
 import { Footer } from "./Footer";
 import { Header } from "./Header";
@@ -9,12 +10,17 @@ import { LayoutErrorBoundary } from "./LayoutErrorBoundary";
  */
 export const FixedLayout: CustomLayout = (page) => {
   return (
-    <div className="grid grid-rows-[auto,1fr,auto] mx-auto max-w-screen-md min-h-screen">
+    <div className="grid grid-cols-1 gap-x-4 px-2 pb-4 mx-auto min-h-screen bg-[#e7e0cf] md:grid-cols-[25%,1fr,25%] md:px-0">
       <Header />
-      <main className="bg-blue-300">
+
+      <main className="pt-[150px] md:overflow-y-auto md:pt-0">
         <LayoutErrorBoundary>{page}</LayoutErrorBoundary>
       </main>
-      <Footer />
+      {/* 後でコンポーネント書き出す */}
+      <div className="bg-white">
+        <Profile />
+        <Footer />
+      </div>
     </div>
   );
 };
