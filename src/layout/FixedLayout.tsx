5ce8d0a2b53f99949e30066bf1c85a35806a68ed
yoshiaki44 <yo44ch@gmail.com>
2022-03-15 08:04:09 +0900

    デザイン微調整

--- src/layout/FixedLayout.tsx
+++ src/layout/FixedLayout.tsx
@@ -10,20 +10,22 @@ import { LayoutErrorBoundary } from "./LayoutErrorBoundary";
  */
 export const FixedLayout: CustomLayout = (page) => {
   return (
-    <div className="grid grid-cols-1 gap-x-4 mx-auto min-h-screen font-sans md:grid-cols-[auto,1fr,25%] md:px-0">
+    <div>
       <p className="fixed top-0 right-0 left-0 z-30 px-2 font-bold text-center bg-yellow-400">
         テスト版のため、実際には購入できません。
       </p>
-      <div className="bg-white border-b border-gray-100 shadow-md">
-        <Header />
-      </div>
-      <main className="pt-[150px] min-h-screen bg-gray-100 md:overflow-y-auto md:pt-0">
-        <LayoutErrorBoundary>{page}</LayoutErrorBoundary>
-      </main>
-      {/* 後でコンポーネント書き出す */}
-      <div className="text-white bg-gray-700 border-l border-white">
-        <Profile />
-        <Footer />
+      <div className="grid grid-cols-1 mx-auto min-h-screen font-sans md:grid-cols-[auto,1fr,25%] md:px-0 md:pt-[24px]">
+        <div className="h-full bg-white border-b border-gray-100 shadow-md">
+          <Header />
+        </div>
+        <main className="pt-[150px] min-h-screen bg-gray-100 md:overflow-y-auto md:pt-0">
+          <LayoutErrorBoundary>{page}</LayoutErrorBoundary>
+        </main>
+        {/* 後でコンポーネント書き出す */}
+        <div className="text-white bg-gray-700 border-l border-white">
+          <Profile />
+          <Footer />
+        </div>
       </div>
     </div>
   );
