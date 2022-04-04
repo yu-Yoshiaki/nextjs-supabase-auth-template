9f0f3391be5be1594858a5a8a1a777f795663d7d
yoshiaki <yoshiosmile68@gmail.com>
2022-04-05 00:47:48 +0900

    型変更

--- src/hook/useFetchTicket.ts
+++ src/hook/useFetchTicket.ts
@@ -3,10 +3,10 @@ import { setDoc } from "firebase/firestore";
 import { collection, doc } from "firebase/firestore";
 import { useCallback } from "react";
 import { firestore, ticketConverter } from "src/lib/firebase";
-import type { CreateTicket, Ticket } from "src/type/ticket";
+import type { ReadTicket, WriteTicket } from "src/type/ticket";
 
 export const useFetchTicket = () => {
-  const createDoc = useCallback(async (ticket: CreateTicket) => {
+  const createDoc = useCallback(async (ticket: WriteTicket) => {
     const collectionRef = collection(firestore, "ticket").withConverter(ticketConverter);
 
     await addDoc(collectionRef, ticket);
@@ -14,7 +14,7 @@ export const useFetchTicket = () => {
     return;
   }, []);
 
-  const updateDoc = useCallback(async (ticket: Ticket) => {
+  const updateDoc = useCallback(async (ticket: ReadTicket) => {
     const docRef = doc(firestore, "ticket", ticket.id);
 
     const review = await setDoc(docRef, ticket, { merge: true });
