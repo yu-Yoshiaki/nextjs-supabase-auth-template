04c09f6ce7d86c22cc4389958a41ec830c97f63f
yoshiaki <yoshiosmile68@gmail.com>
2022-04-05 23:32:16 +0900

    next/jest(Next.js 12.1)から不要になった処理の削除

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,12 +12,18 @@ module.exports = {
   ],
   rules: {
     "no-console": ["error", { allow: ["warn", "info", "error"] }],
-    "no-restricted-syntax": ["error", { selector: "TSEnumDeclaration", message: "Don't declare enums" }],
+    "no-restricted-syntax": [
+      "error",
+      { selector: "TSEnumDeclaration", message: "Don't declare enums" },
+    ],
     "prefer-arrow-callback": "error",
     "prefer-const": "error",
     "func-style": ["error", "expression"],
     "arrow-body-style": ["error", "always"],
-    "no-restricted-imports": ["error", { paths: [{ name: "react", importNames: ["default"] }] }],
+    "no-restricted-imports": [
+      "error",
+      { paths: [{ name: "react", importNames: ["default"] }] },
+    ],
     "react/prop-types": "off",
     "react/react-in-jsx-scope": "off",
     "react/display-name": "error",
@@ -41,8 +47,14 @@ module.exports = {
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-var-requires": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/consistent-type-imports": ["warn", { prefer: "type-imports" }],
-    "@typescript-eslint/no-unused-vars": ["error", { varsIgnorePattern: "^_", argsIgnorePattern: "^_" }],
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      { prefer: "type-imports" },
+    ],
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { varsIgnorePattern: "^_", argsIgnorePattern: "^_" },
+    ],
     "@typescript-eslint/naming-convention": [
       "error",
       { selector: ["typeAlias", "typeParameter"], format: ["PascalCase"] },
@@ -58,7 +70,11 @@ module.exports = {
     "jsx-a11y/no-autofocus": "off",
     "jsx-a11y/anchor-is-valid": [
       "error",
-      { components: ["Link"], specialLink: ["hrefLeft", "hrefRight"], aspects: ["invalidHref", "preferButton"] },
+      {
+        components: ["Link"],
+        specialLink: ["hrefLeft", "hrefRight"],
+        aspects: ["invalidHref", "preferButton"],
+      },
     ],
   },
   overrides: [
@@ -69,8 +85,16 @@ module.exports = {
         "@typescript-eslint/naming-convention": [
           "error",
           { selector: ["typeAlias", "typeParameter"], format: ["PascalCase"] },
-          { selector: ["classProperty", "typeProperty", "method"], format: ["camelCase"] },
-          { selector: "variable", types: ["boolean"], format: ["PascalCase"], prefix: ["is", "has", "should"] },
+          {
+            selector: ["classProperty", "typeProperty", "method"],
+            format: ["camelCase"],
+          },
+          {
+            selector: "variable",
+            types: ["boolean"],
+            format: ["PascalCase"],
+            prefix: ["is", "has", "should"],
+          },
         ],
       },
     },
@@ -81,7 +105,12 @@ module.exports = {
           "error",
           { selector: ["typeAlias", "typeParameter"], format: ["PascalCase"] },
           { selector: ["classProperty", "method"], format: ["camelCase"] },
-          { selector: "variable", types: ["boolean"], format: ["PascalCase"], prefix: ["is", "has", "should"] },
+          {
+            selector: "variable",
+            types: ["boolean"],
+            format: ["PascalCase"],
+            prefix: ["is", "has", "should"],
+          },
         ],
       },
     },
