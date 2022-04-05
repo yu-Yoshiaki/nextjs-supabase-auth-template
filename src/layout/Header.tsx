11a2a9e7016248573757de4d6ac69d4989dbdd9b
yoshiaki <yoshiosmile68@gmail.com>
2022-04-05 23:58:57 +0900

    高さ調整

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,9 +1,8 @@
 import Link from "next/link";
 import type { VFC } from "react";
 import { NavLink } from "src/component/Button";
-import { ProfileSP } from "src/layout/ProfileSP";
 
-const items = [
+export const items = [
   { href: "/", label: "チケット一覧" },
   { href: "#", label: "プライバシーポリシー" },
   { href: "#", label: "利用規約" },
@@ -15,16 +14,14 @@ const items = [
 export const Header: VFC = () => {
   return (
     <header className="fixed inset-x-0 top-6 z-30 pb-1 min-w-[256px] bg-white md:flex md:justify-between md:items-center md:border-b">
-      <div className="flex justify-between items-center border-b md:border-none">
+      <div className="flex justify-between items-center h-[60px] border-b md:border-none">
         <Link href={"/"}>
-          <a className="flex items-center pl-2">
+          <a className="flex items-center pl-2 min-h-30">
             <h1 className="text-2xl font-bold md:text-6xl ">
               TICKET<span className="">tia</span>
             </h1>
           </a>
         </Link>
-
-        <ProfileSP />
       </div>
 
       <nav className="whitespace-nowrap border-b md:border-none">
