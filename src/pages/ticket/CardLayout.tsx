761a188dca716f261d383485be478cd0c82538be
yoshiaki <yoshiosmile68@gmail.com>
2022-04-06 00:02:44 +0900

    デザイン変更

--- src/pages/ticket/CardLayout.tsx
+++ src/pages/ticket/CardLayout.tsx
@@ -3,29 +3,51 @@ import Link from "next/link";
 import type { VFC } from "react";
 import type { ReadTicket } from "src/type/ticket";
 
-export const CardLayout: VFC<{ ticket: ReadTicket }> = (props) => {
+export const CardLayout: VFC<{ ticket: ReadTicket; index: number }> = (
+  props
+) => {
   return (
-    <div className="group relative min-h-[380px] border shadow-lg">
+    <div
+      key={props.ticket.id}
+      className={`group relative shadow-lg border ${
+        props.index === 0
+          ? "col-span-2 h-[380px] mx-5 md:col-span-1 md:mx-0 md:h-[400px] md:w-[420px]"
+          : "min-h-[250px] md:h-[400px] md:w-[420px]"
+      }`}
+    >
       <div className="w-full rounded-md group-hover:opacity-75 ">
         <Link href={`/ticket/${props.ticket.id}`}>
           <a>
-            <Image
-              src={
-                "https://firebasestorage.googleapis.com/v0/b/ticketia-b34da.appspot.com/o/pexels-canva-studio-3194524.webp?alt=media&token=741b46e3-3cee-4c50-8831-32adca4cae01"
-              }
-              alt={""}
-              width={420}
-              height={300}
-              className="block object-cover object-center w-full h-full"
-            />
+            <div className={`${props.index === 0 && "p-2 md:p-0"} relative`}>
+              <Image
+                src={"/mobile.jpg"}
+                alt={""}
+                width={420}
+                height={300}
+                className="block object-cover object-center"
+              />
+            </div>
+            <p
+              className={`absolute text-lg px-2 rounded-sm ${
+                props.index === 0
+                  ? "top-3 left-3 bg-pink"
+                  : "top-1 left-1 bg-accent"
+              }`}
+            >
+              {props.index === 0 ? "締切間近" : props.ticket.start}
+            </p>
+            <div className="flex flex-col p-[10px] space-y-1">
+              <h2 className="text-lg font-bold">{props.ticket.name}</h2>
+              <p className="px-1 text-sm truncate md:text-lg md:text-ellipsis">
+                {props.ticket.description}
+              </p>
+              <p className="px-1 text-xl text-right ">
+                pay {props.ticket.priceList.nomal.price}~
+              </p>
+            </div>
           </a>
         </Link>
       </div>
-      <div className="flex flex-col p-[10px] space-y-1">
-        <h2 className="text-lg font-medium">{props.ticket.name}</h2>
-        <p className="px-1">{props.ticket.description}</p>
-      </div>
-      <p className="px-1 text-xl text-right">pay {props.ticket.priceList.nomal.price}~</p>
     </div>
   );
 };
--- src/pages/ticket/DetailPageLayout.tsx
+++ src/pages/ticket/DetailPageLayout.tsx
@@ -3,18 +3,38 @@ import type { VFC } from "react";
 // import { useEffect, useState } from "react";
 import type { ReadTicket } from "src/type/ticket";
 
-const Checkout: VFC<{ priceList: ReadTicket["priceList"]; stripePriceId: ReadTicket["stripePriceId"] }> = (props) => {
+const Checkout: VFC<{
+  name: ReadTicket["name"];
+  priceList: ReadTicket["priceList"];
+  stripePriceId: ReadTicket["stripePriceId"];
+}> = (props) => {
   return (
-    <div className="flex justify-between items-center">
-      <p className="text-3xl font-bold ">JPY {props.priceList.nomal.price}</p>
+    <div className="pb-5 space-y-5 border border-gray">
+      <p className="p-2 w-full text-left bg-skyblue">配信</p>
+      <h3 className="px-3 text-xl font-bold text-left">{props.name}</h3>
+      <div className="flex justify-between mr-1 ml-3">
+        <p className="py-1 px-2 w-[60px] text-sm bg-pink rounded-full ">
+          販売中
+        </p>
+        <p className="text-2xl font-bold text-right">
+          {props.priceList.nomal.price}円
+          <span className="text-sm">（税込）</span>
+        </p>
+      </div>
 
       <form
         method={"POST"}
         action={`/api/checkout_session/${props.stripePriceId}`}
         className="flex justify-center items-center"
       >
-        <button type="submit" role="link" className={"flex justify-center items-center py-4 px-16 bg-blue"}>
-          Checkout
+        <button
+          type="submit"
+          role="link"
+          className={
+            "flex justify-center items-center py-4 px-20  text-white bg-blue"
+          }
+        >
+          チケット購入
         </button>
       </form>
     </div>
@@ -23,7 +43,7 @@ const Checkout: VFC<{ priceList: ReadTicket["priceList"]; stripePriceId: ReadTic
 
 const DateAndTime = () => {
   return (
-    <div className="py-5 px-2 space-y-2 bg-gray">
+    <div className="py-5 px-2 space-y-2 bg-skyblue">
       <div className="flex space-x-2">
         <p className="font-bold">開催日</p>
         <p>8/20</p>
@@ -36,11 +56,17 @@ const DateAndTime = () => {
   );
 };
 
-const Overview: VFC<{ name: ReadTicket["name"]; description: ReadTicket["description"] }> = (props) => {
+const Overview: VFC<{
+  name: ReadTicket["name"];
+  description: ReadTicket["description"];
+}> = (props) => {
   return (
     <div>
       <h1 className="py-5 text-4xl font-extrabold text-left">{props.name}</h1>
-      <p className=" p-10 w-full min-h-[120px] text-lg border border-gray md:p-20">{props.description}</p>
+      <DateAndTime />
+      <p className=" p-10 mt-5 w-full min-h-[120px] text-lg border border-gray md:p-20">
+        {props.description}
+      </p>
     </div>
   );
 };
@@ -56,9 +82,12 @@ const Organizer = () => {
       <ul className="space-y-3">
         {datas.map((data) => {
           return (
-            <li key={data.title} className="flex space-x-10 border-b border-pink">
-              <p className="w-[80px] text-xl text-left">{data.title}</p>
-              <p className="text-xl">{data.value}</p>
+            <li
+              key={data.title}
+              className="flex space-x-10 border-b border-pink"
+            >
+              <p className="w-[70px] text-xl text-left">{data.title}</p>
+              <p className="text-xl text-left">{data.value}</p>
             </li>
           );
         })}
@@ -67,7 +96,9 @@ const Organizer = () => {
   );
 };
 
-export const DetailPageLayout: VFC<{ ticket: ReadTicket; test: boolean }> = (props) => {
+export const DetailPageLayout: VFC<{ ticket: ReadTicket; test: boolean }> = (
+  props
+) => {
   return (
     <div className="pb-4 tracking-wide leading-relaxed">
       <div className="pt-6 text-center">
@@ -81,9 +112,16 @@ export const DetailPageLayout: VFC<{ ticket: ReadTicket; test: boolean }> = (pro
 
         {/* Product info */}
         <div className="px-4 pb-16 mx-auto space-y-10 max-w-2xl sm:px-6 ">
-          <Overview name={props.ticket.name} description={props.ticket.description} />
-          <DateAndTime />
-          <Checkout priceList={props.ticket.priceList} stripePriceId={props.ticket.stripePriceId} />
+          <Overview
+            name={props.ticket.name}
+            description={props.ticket.description}
+          />
+
+          <Checkout
+            name={props.ticket.name}
+            priceList={props.ticket.priceList}
+            stripePriceId={props.ticket.stripePriceId}
+          />
           {/* {props.ticket.address && <Address address={props.ticket.address} />} */}
           <Organizer />
         </div>
