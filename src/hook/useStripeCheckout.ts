911bd8b2568293eb0e15b9671dfe069c839e9f97
yoshiaki <yoshiosmile68@gmail.com>
2022-04-22 22:54:52 +0900

    変更　型修正, 支払いモードを固定化

--- src/hook/useStripeCheckout.ts
+++ src/hook/useStripeCheckout.ts
@@ -1,14 +1,14 @@
 import axios from "axios";
 import { useCallback } from "react";
-import type stripe from "stripe";
+import type { Stripe } from "stripe";
 
 export const useStripeCheckout = () => {
   const productEndPoint = "/api/checkout/";
 
   const createCheckoutSession = useCallback(
     async (params: {
-      mode: stripe.Checkout.SessionCreateParams.Mode;
-      lineItems: stripe.Checkout.SessionCreateParams.LineItem[];
+      // mode: stripe.Checkout.SessionCreateParams.Mode;
+      lineItems: Stripe.Checkout.SessionCreateParams.LineItem[];
     }) => {
       const res = await axios.post(productEndPoint + "createSession", params);
       const data: { sessionUrl: string } = await res.data;
