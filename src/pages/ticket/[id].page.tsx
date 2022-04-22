b4e5afa9980691e60b9654d4f22bd72010d86f27
yoshiaki <yoshiosmile68@gmail.com>
2022-04-22 23:17:44 +0900

    修正＆追加

--- src/pages/ticket/[id].page.tsx
+++ src/pages/ticket/[id].page.tsx
@@ -1,9 +1,11 @@
 import { loadStripe } from "@stripe/stripe-js";
+import axios from "axios";
 import { collection, doc, getDoc, getDocs } from "firebase/firestore";
 import type { CustomNextPage, GetStaticPaths, GetStaticProps } from "next";
+import { useCallback, useEffect, useState } from "react";
 import { Layout } from "src/layout";
 import { firestore, ticketConverter } from "src/lib/firebase";
-import type { ReadTicket } from "src/type/ticket";
+import type { ReadPrice, ReadTicket } from "src/type/ticket";
 
 import { DetailPageLayout } from "./layout/DetailPageLayout";
 
@@ -11,8 +13,9 @@ loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string);
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const colRef = collection(firestore, "ticket").withConverter(ticketConverter);
-  const document = await getDocs(colRef);
-  const paths = document.docs.map((data) => {
+  const documents = await getDocs(colRef);
+
+  const paths = documents.docs.map((data) => {
     return { params: { id: data.id } };
   });
 
@@ -24,23 +27,21 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
 export const getStaticProps: GetStaticProps = async (paths) => {
   if (paths.params) {
-    const colRef = doc(
+    const docRef = doc(
       firestore,
       "ticket",
       paths.params.id as string
     ).withConverter(ticketConverter);
-    const document = await getDoc(colRef);
 
-    const posts = JSON.parse(JSON.stringify(document.data()));
-    // const posts = document.data();
-    if (document) {
-      return {
-        props: {
-          posts,
-        },
-        revalidate: 5,
-      };
-    }
+    const document = await getDoc(docRef);
+    const data = JSON.parse(JSON.stringify(document.data()));
+
+    return {
+      props: {
+        data,
+      },
+      revalidate: 5,
+    };
   }
 
   return {
@@ -48,8 +49,21 @@ export const getStaticProps: GetStaticProps = async (paths) => {
   };
 };
 
-const Index: CustomNextPage<{ posts: ReadTicket }> = (props) => {
-  return <DetailPageLayout ticket={props.posts} test={false} />;
+const Index: CustomNextPage<{ data: ReadTicket }> = (props) => {
+  // console.log("front", props.prices);
+  const [prices, setPrices] = useState<ReadPrice[]>();
+
+  const fetchPrices = useCallback(async () => {
+    const res = await axios.get(`/api/fb/price/${props.data.id}/get`);
+    const prices: ReadPrice[] = await res.data;
+    setPrices(prices);
+  }, [props.data.id]);
+
+  useEffect(() => {
+    fetchPrices();
+  }, [fetchPrices]);
+
+  return <DetailPageLayout ticket={props.data} test={false} prices={prices} />;
 };
 
 Index.getLayout = Layout;
