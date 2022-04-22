f0eb3076cabf8c2ab5490f7598b5b169cc4ef990
yoshiaki <yoshiosmile68@gmail.com>
2022-04-22 23:21:05 +0900

    変更

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -2,11 +2,18 @@ import { collection, getDocs } from "firebase/firestore";
 import type { CustomNextPage, GetStaticProps } from "next";
 import { Layout } from "src/layout";
 import { firestore, ticketConverter } from "src/lib/firebase";
-import { TicketList } from "src/pages/ticket/layout/TicketList";
 import type { ReadTicket } from "src/type/ticket";
 
+import { CardLayout } from "./ticket/layout";
+
 const Root: CustomNextPage<{ posts: ReadTicket[] }> = (props) => {
-  return <TicketList datas={props.posts} test={false} />;
+  return (
+    <div className="grid grid-cols-2 gap-x-2 gap-y-10 px-2 mt-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
+      {props.posts.map((data, index) => {
+        return <CardLayout ticket={data} key={data.id} index={index} />;
+      })}
+    </div>
+  );
 };
 
 export const getStaticProps: GetStaticProps = async () => {
@@ -18,6 +25,7 @@ export const getStaticProps: GetStaticProps = async () => {
   const data = docDatas.docs.map((d) => {
     return d.data();
   });
+
   // DateTime型のデータをそのまま読み込もうとするとエラーが起こるっぽい
   // JSONにシリアライズ可能なデータ型のみを返す
   const posts = JSON.parse(JSON.stringify(data));
