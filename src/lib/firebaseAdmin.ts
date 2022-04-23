2a90c1f1496f77312584a9efa19ad17d520a8690
yoshiaki <yoshiosmile68@gmail.com>
2022-04-23 09:34:34 +0900

    変更　FirebaseAdminのAPIKEYをjsonファイル→環境変数に

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -1,9 +1,14 @@
+import type { ServiceAccount } from "firebase-admin";
 import * as admin from "firebase-admin";
-import { applicationDefault, initializeApp } from "firebase-admin/app";
 
+const cert: ServiceAccount = {
+  privateKey: process.env.FIREBASE_KEY?.replace(/\\n/g, "\n"),
+  projectId: process.env.FIREBASE_PROJECTID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+};
 //初期化が複数発生し、エラーになるため、分岐で回避
 if (admin.apps.length === 0) {
-  initializeApp({
-    credential: applicationDefault(),
+  admin.initializeApp({
+    credential: admin.credential.cert(cert),
   });
 }
