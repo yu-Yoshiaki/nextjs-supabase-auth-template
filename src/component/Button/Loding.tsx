4202a7422b419aec8e87b4bd837c9447440aede7
yoshiaki <yoshiosmile68@gmail.com>
2022-05-03 00:36:02 +0900

    削除　移動

--- src/component/Button/Loding.tsx
+++
@@ -1,9 +0,0 @@
-import type { VFC } from "react";
-
-export const Loding: VFC = () => {
-  return (
-    <div className="flex fixed inset-0 z-10 justify-center items-center bg-black opacity-50">
-      <p className="text-2xl text-white">Loding...</p>
-    </div>
-  );
-};
