76d44b7aafbcbc5664cbbe2c654ce0072b1382b4
yoshiaki <yoshiosmile68@gmail.com>
2022-05-03 00:39:29 +0900

    変更　SWR化

--- src/pages/ticket/layout/DetailPageLayout.tsx
+++ src/pages/ticket/layout/DetailPageLayout.tsx
@@ -1,44 +1,43 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 import type { VFC } from "react";
 import type { ReadTicket } from "src/type/ticket";
+import useSWR from "swr";
 
 import { Address } from "./Address";
 import { Checkout } from "./Checkout";
 import { Organizer } from "./Organizer";
 import { Overview } from "./Overview";
 
-type DetailPageLayout = {
-  ticket: ReadTicket;
-  test: boolean;
-};
+export const DetailPageLayout: VFC = () => {
+  const router = useRouter();
+  const { id } = router.query;
 
-export const DetailPageLayout: VFC<DetailPageLayout> = (props) => {
+  const { data: ticket } = useSWR<ReadTicket>(id as string);
+
   return (
     <div className="pb-4 tracking-wide leading-relaxed">
-      <div className="pt-6 text-center">
-        <Image
-          width={800}
-          height={480}
-          src={props.ticket.images[0] ?? "/noimage.jpg"}
-          alt={props.ticket.images[0] ? props.ticket.name : "not image data"}
-          className="object-cover object-center w-full h-full"
-        />
-
-        {/* Product info */}
-        <div className="px-4 pb-16 mx-auto space-y-10 max-w-2xl sm:px-6 ">
-          <Overview
-            name={props.ticket.name}
-            description={props.ticket.description}
+      {ticket && (
+        <div className="pt-6 text-center">
+          <Image
+            width={800}
+            height={480}
+            src={ticket.images[0] ?? "/noimage.jpg"}
+            alt={ticket.images[0] ? ticket.name : "not image data"}
+            className="object-cover object-center w-full h-full"
           />
 
-          <Checkout name={props.ticket.name} id={props.ticket.id} />
+          {/* Product info */}
+          <div className="px-4 pb-16 mx-auto space-y-10 max-w-2xl sm:px-6 ">
+            <Overview name={ticket.name} description={ticket.description} />
 
-          {props.ticket.metadata.address && (
-            <Address data={props.ticket.metadata} />
-          )}
-          <Organizer />
+            <Checkout />
+
+            {ticket.metadata.address && <Address data={ticket.metadata} />}
+            <Organizer />
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
