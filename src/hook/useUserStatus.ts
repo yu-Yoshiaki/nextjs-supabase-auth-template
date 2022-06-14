9d5a15b64ec3190b62c0f8210d29b2d1074daf97
yoshiaki <yoshiosmile68@gmail.com>
2022-06-14 20:07:13 +0900

    変更

--- src/hook/useUserStatus.ts
+++ src/hook/useUserStatus.ts
@@ -4,6 +4,7 @@
 import type { User } from "firebase/auth";
 import { onAuthStateChanged } from "firebase/auth";
 import { getAuth } from "firebase/auth";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { app } from "src/lib/firebase";
 
@@ -11,13 +12,14 @@ const auth = getAuth(app);
 
 export const useUserStatus = () => {
   const [user, setUser] = useState<User | null>(null);
-
+  const router = useRouter();
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         setUser(user);
       } else {
         setUser(null);
+        router.push("/");
       }
     });
 
