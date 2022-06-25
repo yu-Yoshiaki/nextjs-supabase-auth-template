92e011e88b4d00c1d27cb8f493abf533d023580d
yoshiaki <yoshiosmile68@gmail.com>
2022-06-25 18:19:39 +0900

    調整

--- src/layout/HeaderPC.tsx
+++ src/layout/HeaderPC.tsx
@@ -1,41 +1,38 @@
 import Link from "next/link";
 import { memo } from "react";
 import { SmartPhoneMenu } from "src/component";
-import { NavLink } from "src/component/Button";
-
-export const items = [{ href: "/", label: "チケット一覧" }];
-
+import { useUserStatus } from "src/hook/useUserStatus";
 /**
  * @package
  */
 
 // eslint-disable-next-line react/display-name
 export const HeaderPC = memo(() => {
+  const { user } = useUserStatus();
   return (
-    <header className="fixed inset-x-0 top-6 z-30 pb-1 min-w-[256px] md:flex md:justify-between md:items-center">
-      <div className="flex justify-between items-center h-[90px]">
-        <Link href={"/"}>
-          <a className="flex items-center pl-2">
-            <h1 className="text-2xl font-bold tracking-[-0.1em]">
-              チケットマーケット
-            </h1>
-          </a>
-        </Link>
-      </div>
+    <header className="fixed inset-x-0 top-0 z-10 w-full bg-white">
+      <div className="mx-auto w-[80%] h-[80px] md:flex md:justify-between md:items-center">
+        <div className="flex justify-between items-center">
+          <Link href={"/"}>
+            <a className="flex items-center pl-2">
+              <h1 className="text-2xl font-bold tracking-[-0.1em]">
+                チケットマーケット
+              </h1>
+            </a>
+          </Link>
+        </div>
 
-      <div className="flex items-center">
-        <nav className="whitespace-nowrap md:border-none">
-          {items.map(({ href, label }) => {
-            return (
-              <NavLink key={href} href={href} activeClassName="text-red-500">
-                <a className="inline-block py-2 px-4 hover:bg-blue-400">
-                  {label}
-                </a>
-              </NavLink>
-            );
-          })}
-        </nav>
-        <SmartPhoneMenu />
+        <div className="flex items-center">
+          {!user ? (
+            <Link href={"/consumer/auth/login"} passHref>
+              <a className="py-2 px-4 text-sm text-white bg-blue-300 hover:bg-blue-200 rounded-full">
+                ログイン / 新規作成
+              </a>
+            </Link>
+          ) : (
+            <SmartPhoneMenu />
+          )}
+        </div>
       </div>
     </header>
   );
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,28 +1,20 @@
 import type { CustomLayout } from "next";
-import { DontPurchaseAlert } from "src/component";
 import { useGetWindowSize } from "src/hook/useGetWindowSize";
 
 import { Footer } from "./Footer";
 import { HeaderPC } from "./HeaderPC";
 import { HeaderSP } from "./HeaderSP";
 import { LayoutErrorBoundary } from "./LayoutErrorBoundary";
-/**
- * @package
- */
+
 export const Layout: CustomLayout = (page) => {
   const { windowSize } = useGetWindowSize();
   return (
     <div>
-      <DontPurchaseAlert />
-      <div className="grid grid-cols-1 mx-auto mb-5 min-h-screen md:grid-cols-[auto,1fr] md:px-0 md:pt-[50px]">
-        <div className="h-full">
-          {windowSize.width > 480 ? <HeaderPC /> : <HeaderSP />}
-        </div>
-        <main className="pt-[100px] min-h-screen md:overflow-y-auto md:pt-[60px]">
-          <LayoutErrorBoundary>{page}</LayoutErrorBoundary>
-        </main>
-      </div>
-      <Footer />
+      {windowSize.width > 480 ? <HeaderPC /> : <HeaderSP />}
+      <main className="mt-[80px] min-h-[calc(100vh-160px)] bg-blue-50">
+        <LayoutErrorBoundary>{page}</LayoutErrorBoundary>
+      </main>
+      <Footer />{" "}
     </div>
   );
 };
