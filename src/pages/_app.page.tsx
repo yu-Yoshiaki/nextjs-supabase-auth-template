4aff84a3eaddf86615d3600369bb4565c272e2bd
yoshiaki <yoshiosmile68@gmail.com>
2022-07-10 20:20:36 +0900

    追加　カート機能

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -2,6 +2,7 @@ import "tailwindcss/tailwind.css";
 
 import type { CustomAppPage } from "next/app";
 import Head from "next/head";
+import { CartProvider } from "use-shopping-cart";
 
 const App: CustomAppPage = (props) => {
   const getLayout =
@@ -12,22 +13,31 @@ const App: CustomAppPage = (props) => {
 
   return (
     <>
-      <Head>
-        <title>チケットマーケット</title>
-        <meta property="og:url" content="https://ticketia.vercel.app/" />
-        <meta property="og:type" content="product" />
-        <meta property="og:title" content="チケットマーケット" />
-        <meta
-          property="og:description"
-          content="チケット売買アプリケーション"
-        />
-        <meta
-          property="og:site_name"
-          content="ホームページのコーディング ポートフォリオ"
-        />
-        <meta property="og:image" content="/noimage.jpg" />
-      </Head>
-      {getLayout(<props.Component {...props.pageProps} />)}
+      <CartProvider
+        mode="payment"
+        cartMode="client-only"
+        stripe={process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string}
+        currency="JPY"
+        successUrl="http://localhost:3000"
+        cancelUrl="http://localhost:3000"
+      >
+        <Head>
+          <title>チケットマーケット</title>
+          <meta property="og:url" content="https://ticketia.vercel.app/" />
+          <meta property="og:type" content="product" />
+          <meta property="og:title" content="チケットマーケット" />
+          <meta
+            property="og:description"
+            content="チケット売買アプリケーション"
+          />
+          <meta
+            property="og:site_name"
+            content="ホームページのコーディング ポートフォリオ"
+          />
+          <meta property="og:image" content="/noimage.jpg" />
+        </Head>
+        {getLayout(<props.Component {...props.pageProps} />)}
+      </CartProvider>
     </>
   );
 };
